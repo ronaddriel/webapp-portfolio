edecf9c14b47d147f15fd654d2c981552071a2ec
Ron <ronaddrielm@gmail.com>
2026-07-14 06:17:05 -0400

    Add multi-page build entrypoints for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
-import tailwindcss  from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        home: resolve(__dirname, "home.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+        resume: resolve(__dirname, "resume.html"),
+        project1: resolve(__dirname, "1.html"),
+        project2: resolve(__dirname, "2.html"),
+        project3: resolve(__dirname, "3.html"),
+        project4: resolve(__dirname, "4.html"),
+        project5: resolve(__dirname, "5.html"),
+      },
+    },
+  },
 });
